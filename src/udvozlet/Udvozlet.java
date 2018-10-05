4026a169be1dd9a75b630f73221e4af6a43ff820
szoftverfejleszto2016 <szoftverfejleszto2016@gmail.com>
2018-10-05 17:51:02 +0200

    Első változat. Egy osztály.

---
+++ src/udvozlet/Udvozlet.java
@@ -0,0 +1,16 @@
+package udvozlet;
+
+/**
+ *
+ * @author tothj
+ */
+public class Udvozlet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Üdvözöllek!");
+    }
+    
+}
