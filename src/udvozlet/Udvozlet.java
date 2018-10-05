1701d2c8c143acc379c8d891408059a8e5cac3a5
infotanar <34937202+szoftverfejleszto2016@users.noreply.github.com>
2018-10-05 18:09:59 +0200

    új nevek

--- src/udvozlet/Udvozlet.java
+++ src/udvozlet/Udvozlet.java
@@ -10,8 +10,8 @@ public class Udvozlet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Ember no = new Ember("Éva");
-        Ember ferfi = new Ember("Ádám");
+        Ember no = new Ember("Lujza");
+        Ember ferfi = new Ember("Géza");
         
         System.out.println(no.koszont());
         System.out.println(ferfi.koszont());
